0d52caac1f503226f82afc8fde156794ec56e9d7
Guilherme <guilherme.pcruz@hotmail.com>
2021-01-22 20:44:58 -0300

    Classes

--- javascript/06-classes.js
+++ javascript/06-classes.js
@@ -1,3 +1,4 @@
+/** /
 class MyClass{
     constructor(){
         metodo01(){
@@ -8,3 +9,34 @@ class MyClass{
         }
     }
 }
+
+const myConst = class MyClass{}
+
+// ou
+
+const MyConst2 = class {}
+
+// ou
+
+function MakeClass(){
+    return class{}
+}
+
+/**/
+
+
+class Dog{
+    constructor( name ){
+        this._name = name
+    }
+}
+
+const Pet = new Dog('Nina')
+
+console.log(Pet)
+/*
+const Belinha = new Dog()
+
+console.log(Belinha)
+
+/**/
